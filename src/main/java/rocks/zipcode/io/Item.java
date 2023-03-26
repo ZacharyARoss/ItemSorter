b057e2228b8d35adfa350d0d2b1e642939a6bad8
Zachary <zachary@Zipcoders-MBP-2.fios-router.home>
2023-03-26 18:39:03 -0400

    finished item method and getters and setters

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -3,31 +3,52 @@ package rocks.zipcode.io;
 /**
  * @author leon on 30/01/2019.
  */
-public class Item {
-    public Item() {
+public class Item
+{
+    private Long id;
+    private String name;
+    private Double price;
+    public Item()
+    {
+        this.id = 0L;
+        this.name = "";
+        this.price = 0.01;
     }
 
-    public Item(Long id, String name, Double price) {
+    public Item(Long id, String name, Double price)
+    {
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
 
-    public String getName() {
-        return null;
+    public String getName()
+    {
+        return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name)
+    {
+        this.name = name;
     }
 
-    public Long getId() {
-        return null;
+    public Long getId()
+    {
+        return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Long id)
+    {
+        this.id = id;
     }
 
-    public Double getPrice() {
-        return null;
+    public Double getPrice()
+    {
+        return price;
     }
 
-    public void setPrice(Double price) {
+    public void setPrice(Double price)
+    {
+        this.price = price;
     }
 }
--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,17 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<IdComparator>
+{
+    @Override
+    public int compare(IdComparator o1, IdComparator o2)
+    {
+        return 0;
+    }
 }
